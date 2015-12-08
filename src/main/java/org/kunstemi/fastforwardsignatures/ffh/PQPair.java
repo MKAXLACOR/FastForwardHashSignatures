786fbc34c330d708bbb0554ad6baf3c179a35064
researchgoal <researchgoalresearchgoal>
2015-12-08 20:13:42 +0100

    Initial commit of source code

--- src/main/java/org/kunstemi/fastforwardsignatures/ffh/PQPair.java
+++ src/main/java/org/kunstemi/fastforwardsignatures/ffh/PQPair.java
@@ -1,4 +1,10 @@
-The MIT License (MIT)
+/**
+ * Copyright 2015 Axlacor Ltd
+ */
+package org.kunstemi.fastforwardsignatures.ffh;
+
+/*
+ * The MIT License
 
 Copyright (c) 2015 Axlacor Ltd, (Michael Kunstel)
 
@@ -9,14 +15,48 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
 
+import java.math.BigInteger;
+
+public class PQPair
+{
+    private BigInteger p;
+    private BigInteger q;
+
+    public BigInteger getP()
+    {
+        return p;
+    }
+
+    public void setP(BigInteger p)
+    {
+        this.p = p;
+    }
+
+    public BigInteger getQ()
+    {
+        return q;
+    }
+
+    public void setQ(BigInteger q)
+    {
+        this.q = q;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return p + ", " + q;
+    }
+}
